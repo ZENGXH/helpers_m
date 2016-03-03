f54864f282d36ecd0b054db404d6ad06c407af3b
zhuwj <wangjiang88119@gmail.com>
2016-03-03 19:59:26 +0800

    modified LoadNDFloat_bin.m, support shape reverse

--- LoadNDFloat_bin.m
+++ LoadNDFloat_bin.m
@@ -1,16 +1,23 @@
-function mat = LoadNDFloat_bin(path_file, ndim, do_wh_permute)
+function mat = LoadNDFloat_bin(path_file, ndim, do_wh_permute, do_dim_reverse)
+if nargin < 4
+    do_dim_reverse = false;
+end
+
 fid = fopen(path_file, 'r');
 if (fid < 0)
     error('Failed to open %s for reading', path_file);
 end
-
 siz = fread(fid, ndim, 'int');
 mat = fread(fid, prod(siz), 'float');
 fclose(fid);
 
 if length(siz) == 1
-    siz = [siz, 1];
+    siz = [1, siz];
 end
+if do_dim_reverse
+	siz = siz(end:-1:1); %original order: num, chn, height, width
+end
+
 mat = reshape(mat, reshape(siz, 1, []));
 if do_wh_permute
     order = 1:length(siz);
